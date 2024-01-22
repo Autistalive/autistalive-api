b86af02843982012298f954cdc5e3a2abb217794
Vinicius149 <vinicius.git14@gmail.com>
2024-01-22 17:01:21 -0300

    Criado banco de dados, tabelas e controllers.

--- database/migrations/2024_01_22_193816_create_users_table.php
+++ database/migrations/2024_01_22_193816_create_users_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->text('firstName');
+            $table->text('lastName');
+            $table->text('city');
+            $table->text('state');
+            
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('users');
     }
 };
