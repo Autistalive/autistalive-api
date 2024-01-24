da407f3ac87f2616e63d3b91abf8bd89cf9f63e7
norththefirst <devnorth@protonmail.com>
2024-01-23 21:44:29 -0300

    feat: migration and couple fixes

--- database/migrations/2024_01_23_212235_create_admin_table.php
+++ database/migrations/2024_01_23_212235_create_admin_table.php
@@ -10,14 +10,11 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('autism_level');
-            $table->string('report_file');
-            $table->string('face_id');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -28,6 +25,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admin');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api', function () {
+    return response()->json(["version" => "0.0.1-dev"]);
+});
