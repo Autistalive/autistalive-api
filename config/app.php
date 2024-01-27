080a0d37479d8584f5449bb9bd37d56d917a9a6c
clys-man <clysmanalves@gmail.com>
2024-01-27 14:02:51 -0300

    feat: add app version config

--- config/app.php
+++ config/app.php
@@ -20,6 +20,17 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used when
+    | the framework needs to place the application's version in a notification
+    | or any other location as required by the application or its packages.
+    */
+    'version' => env('APP_VERSION', '1.0.0'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Application Environment
     |--------------------------------------------------------------------------
     |
