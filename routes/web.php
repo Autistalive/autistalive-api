eb260350a3c7619dbca2c1076db0c05d166a6895
clys-man <clysmanalves@gmail.com>
2024-01-27 14:04:52 -0300

    feat: change app version in web route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Laravel' => config('app.version')];
 });
 
 require __DIR__.'/auth.php';
