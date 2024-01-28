f75cff52af9a887e72218cdd1fe4afc8277e3188
maotovisk <me@maot.dev>
2024-01-28 00:28:54 -0300

    database: add Admin seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Enums\UserStatus;
 use App\Enums\UserSupportLevel;
+use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -28,6 +29,15 @@ class DatabaseSeeder extends Seeder
             'support_level' => UserSupportLevel::HIGH,
             'report_photo' => 'https://kernel.org/theme/images/logos/tux.png', //placeholder
             'face_photo' => 'https://kernel.org/theme/images/logos/tux.png', //placeholder
+        ]);
+
+        /*
+            Test Admin Seeder.
+        */
+        Admin::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'test@test.com',
+            'password' => Hash::make('password'),
         ]);
     }
 }
