767ed5996e2a31466e3c4b7876237030f9cff833
maotovisk <me@maot.dev>
2024-01-28 15:59:15 -0300

    database: fix AdminFactory not working

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -26,7 +26,6 @@ class AdminFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
