3bb82cd0094d47fc5a5a11585c832c8a358f1a96
Roberto Esteves <contact@robertoesteves.dev>
2024-01-28 14:58:44 -0300

    database: Refactor Admins and Users Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Enums\UserStatus;
-use App\Enums\UserSupportLevel;
-use App\Models\Admin;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,27 +11,5 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        /*
-            Test User Seeder.
-        */
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@test.com',
-            'password' => Hash::make('password'),
-            'status' => UserStatus::ACTIVE,
-            'support_level' => UserSupportLevel::HIGH,
-            'report_photo' => 'https://kernel.org/theme/images/logos/tux.png', //placeholder
-            'face_photo' => 'https://kernel.org/theme/images/logos/tux.png', //placeholder
-        ]);
-
-        /*
-            Test Admin Seeder.
-        */
-        Admin::factory()->create([
-            'name' => 'Test Admin',
-            'email' => 'test@test.com',
-            'password' => Hash::make('password'),
-        ]);
     }
 }
